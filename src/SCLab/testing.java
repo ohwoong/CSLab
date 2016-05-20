8a136ba0e53d92a40f78f697dee2a5562021a30a
ohwoong <abcd56789@hanmail.net>
2016-05-20 11:07:58 +0900

    Update testing.java

--- src/SCLab/testing.java
+++ src/SCLab/testing.java
@@ -1,5 +1,21 @@
 package SCLab;
 
-public class testing {
+public class Testing {
+  private String wifi_name;
+  private int wifi_number;
+  
+  class Testing(){};
+  class Testing(wifi_name, wifi_number);
+  
+  class void print()
+  {
+    System.out.println(wifi_name, wifi_number);
+  }
+  
+  public static void main(String[] args)
+  {
+    Testing test = new Testing(DGU, 12345);
+    test.print();
+  }
 
 }
